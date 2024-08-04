72ce6ebf3cde47da4c2f8dfdc2b731745c588181
Amin Jafari <amincorleone81@gmail.com>
2024-08-04 12:39:50 +0330

    initial

--- mysql_writer.py
+++ mysql_writer.py
@@ -36,7 +36,7 @@ def test_mysql_connection():
         )
         logging.info("Successfully connected to MySQL")
         cur = conn.cursor()
-        cur.execute("CREATE TABLE hashes (id serial PRIMARY KEY, hash TEXT, created_at TIMESTAMP);")
+        cur.execute("CREATE TABLE IF NOT EXISTS hashes (id serial PRIMARY KEY, hash TEXT, created_at TIMESTAMP);")
         conn.close()
         return True
     except Exception as e:
