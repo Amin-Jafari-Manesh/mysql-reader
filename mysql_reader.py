383c78e2713aec5d2ea89687b1fbbdb054925da2
Amin Jafari <amincorleone81@gmail.com>
2024-08-07 11:25:51 +0330

    initial

--- mysql_reader.py
+++ mysql_reader.py
@@ -1,5 +1,4 @@
 import logging
-from datetime import datetime
 import time
 from os import environ
 import mysql.connector
@@ -9,43 +8,40 @@ logging.basicConfig(level=logging.INFO)
 db_config = {
     'PASS': environ.get('PASS', ''),
     'DOMAIN': environ.get('DOMAIN', ''),
-    'INSERT_DELAY' : int(environ.get('INSERT_DELAY', '')),
+    'DATA_TYPE': environ.get('DATA_TYPE', ''),
+    'INSERT_DELAY': int(environ.get('INSERT_DELAY', '')),
 }
 
-def test_mysql_connection():
-    try:
-        conn = mysql.connector.connect(
-            host=db_config['DOMAIN'],
-            user='admin',
-            password=db_config['PASS'],
-            database='db',
-            port='3306'
-        )
-        logging.info("Successfully connected to MySQL")
-        conn.close()
-        return True
-    except Exception as e:
-        logging.error(f"MySQL connection failed: {e}")
-        return False
+conn = mysql.connector.connect(
+    host=db_config['DOMAIN'],
+    user='admin',
+    password=db_config['PASS'],
+    database='db',
+    port='3306'
+)
 
 
 def mysql_read() -> bool:
-    if test_mysql_connection():
-        conn = mysql.connector.connect(
-            host=db_config['DOMAIN'],
-            user='admin',
-            password=db_config['PASS'],
-            database='db',
-            port='3306'
-        )
+    if conn.is_connected():
+        logging.info("Connected to the database.")
         cur = conn.cursor()
-        while True:
-            time.sleep(db_config['INSERT_DELAY']*0.001)
-            cur.execute(f'SELECT * FROM hashes ORDER BY "created_at" DESC LIMIT 100')
-            conn.commit()
-        conn.close()
-        return True
-    return False
+        if db_config['DATA_TYPE'] == 'h':
+            while True:
+                time.sleep(db_config['INSERT_DELAY'] * 0.001)
+                cur.execute(f'SELECT * FROM hashes ORDER BY "created_at" DESC LIMIT 100')
+                conn.commit()
+        elif db_config['DATA_TYPE'] == 't':
+            while True:
+                time.sleep(db_config['INSERT_DELAY'] * 0.001)
+                cur.execute(f'SELECT * FROM text ORDER BY "created_at" DESC LIMIT 100')
+                conn.commit()
+        else:
+            logging.error("Invalid data type.")
+            return False
+    else:
+        logging.error("Failed to connect to the database.")
+        return False
+    return True
 
 
 if __name__ == '__main__':
